7125b43a30b8a53801f80031f03b04c8a3ce3357
emmanuel sam <fndm4@gmail.com>
2025-03-23 07:17:17 +0100

    cors error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+// const app = express();
+// const router = require("./routes/route");
+// const PORT = process.env.PORT || 3000;
+
+// mongoose.connect(process.env.MONGO_DB_URL);
+// mongoose.connection.on("open", () => console.log("Handshake Established"));
+// mongoose.connection.on("err", (err) => console.log(err));
+
+// app.use(express.json({ limit: "100mb" }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// /*{
+//     origin: function (origin, callback) {
+//       const allowedOrigins = [
+//         "https://store-server-6lv5.onrender.com",
+//         "http://localhost:5174",
+//         "http://localhost:5173",
+//         "https://store-ruby-tau.vercel.app",
+//       ];
+//       if (
+//         allowedOrigins.includes(origin) ||
+//         origin === undefined ||
+//         origin === null
+//       ) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("CORS policy: Origin not allowed"), false);
+//       }
+//     },
+//     credentials: true,
+//   } */
+// app.use(
+//   cors("*")
+// );
+
+// // app.options("*", cors());
+
+// app.use("/api", router);
+
+// app.listen(PORT, () => {
+//   console.log(`App running on http://localhost:${PORT}`);
+// });
+
+
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -15,7 +65,9 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-/*{
+// CORS configuration
+app.use(
+  cors({
     origin: function (origin, callback) {
       const allowedOrigins = [
         "https://store-server-6lv5.onrender.com",
@@ -34,12 +86,8 @@ app.use(cookieParser());
       }
     },
     credentials: true,
-  } */
-app.use(
-  cors("*")
+  })
 );
-
-// app.options("*", cors());
 
 app.use("/api", router);
 
